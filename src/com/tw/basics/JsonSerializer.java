5818a2e1c797a7babac1152ca02a970832d7a8c9
Riguz Lee <solee.linux@gmail.com>
2019-09-09 17:30:06 +0800

    added an impl for your reference

--- src/com/tw/basics/JsonSerializer.java
+++ src/com/tw/basics/JsonSerializer.java
@@ -1,14 +1,31 @@
 package com.tw.basics;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class JsonSerializer {
 
   public String serialize(JsonObject[] object) {
-    // TODO: serialize object array to Json String
-    throw new UnsupportedOperationException();
+    String items = Stream.of(object)
+        .map(this::serialize)
+        .collect(Collectors.joining(","));
+
+    return String.format("[%s]", items);
   }
 
   public String serialize(JsonObject object) {
-    // TODO: serialize one object to Json String
-    throw new UnsupportedOperationException();
+    String value = object.getValue();
+    if ("date".equals(object.getName())) {
+      value = formatDate(value);
+    }
+    return String.format("{%s:%s}", wrapString(object.getName()), wrapString(value));
+  }
+
+  private String formatDate(String date) {
+    return date.replaceAll("/", "-");
+  }
+
+  private String wrapString(String value) {
+    return "\"" + value + "\"";
   }
 }
--- src/com/tw/basics/NumberConverter.java
+++ src/com/tw/basics/NumberConverter.java
@@ -3,12 +3,18 @@ package com.tw.basics;
 public class NumberConverter {
 
   public int convert(String number) {
-    // TODO: convert a string to number
-    throw new UnsupportedOperationException();
+    return Integer.parseInt(number);
   }
 
   public String convertToString(Number number) {
-    // TODO: convert a number to String
-    throw new UnsupportedOperationException();
+    String value = number.toString();
+    String suffix = "";
+    if (number instanceof Long) {
+      suffix = "L";
+    }
+    if (number instanceof Double) {
+      suffix = "d";
+    }
+    return value + suffix;
   }
 }
